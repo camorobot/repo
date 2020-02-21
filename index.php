d11a07305a10a450b70c99ffd22d08aa2ba6016f
camorobot <41551968+camorobot@users.noreply.github.com>
2020-02-21 22:58:19 +0100

    Initial commit

---
+++ index.php
@@ -0,0 +1,106 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="assets/css/style.css">
+    <title>Camorobot's Repo</title>
+</head>
+<body>
+    <section class="header">
+        <nav class="nav">
+            <ul class="header-left">
+                <li><a href="https://repo.camorobot.nl"> Camorobot</a></li>
+            </ul>
+            <ul class="header-right">
+                <li>Login</li>
+                <li>Aanmelden</li>
+            </ul>
+        </nav>
+    </section>
+    <section class="main">
+        <section class="wrapper-cydia">
+            <div class="div-title">
+                <h1 class="title">Camorobot's Repo</h1>
+            </div>
+            <div class="button">
+                <p class="button-text"><a href="cydia://url/https://cydia.saurik.com/api/share#?source=https://repo.camorobot.nl"></a></p>
+            </div>
+        </section>
+        <section class="wrapper-populaire">
+            <div class="div-title">
+                <h1 class="title">Populaire</h1>
+            </div>
+            <div class="div-cards">
+                <div class="card">
+                    <img src="assets/packages/DEFAULT/icons/icon.png" alt="Teak-icon">
+                    <div class="div-title-card">
+                        <h2 class="title-card">DEFAULT TWEAK</h2>
+                    </div>
+                    <div class="div-info-card">
+                        <h2 class="kind-card">Tweak</h2>
+                        <h2 class="user-card">Camorobot</h2>
+                    </div>
+                </div>
+                <div class="card">
+                    <img src="assets/packages/DEFAULT/icons/icon.png" alt="Teak-icon">
+                    <div class="div-title-card">
+                        <h2 class="title-card">DEFAULT TWEAK</h2>
+                    </div>
+                    <div class="div-info-card">
+                        <h2 class="kind-card">Tweak</h2>
+                        <h2 class="user-card">Camorobot</h2>
+                    </div>
+                </div>
+                <div class="card">
+                    <img src="assets/packages/DEFAULT/icons/icon.png" alt="Teak-icon">
+                    <div class="div-title-card">
+                        <h2 class="title-card">DEFAULT TWEAK</h2>
+                    </div>
+                    <div class="div-info-card">
+                        <h2 class="kind-card">Tweak</h2>
+                        <h2 class="user-card">Camorobot</h2>
+                    </div>
+                </div>
+            </div>
+        </sectopn>
+        <section class="wrapper-New">
+            <div class="div-title">
+                <h1 class="title">Populaire</h1>
+            </div>
+            <div class="div-cards">
+                <div class="card">
+                    <img src="assets/packages/DEFAULT/icons/icon.png" alt="Teak-icon">
+                    <div class="div-title-card">
+                        <h2 class="title-card">DEFAULT TWEAK</h2>
+                    </div>
+                    <div class="div-info-card">
+                        <h2 class="kind-card">Tweak</h2>
+                        <h2 class="user-card">Camorobot</h2>
+                    </div>
+                </div>
+                <div class="card">
+                    <img src="assets/packages/DEFAULT/icons/icon.png" alt="Teak-icon">
+                    <div class="div-title-card">
+                        <h2 class="title-card">DEFAULT TWEAK</h2>
+                    </div>
+                    <div class="div-info-card">
+                        <h2 class="kind-card">Tweak</h2>
+                        <h2 class="user-card">Camorobot</h2>
+                    </div>
+                </div>
+                <div class="card">
+                    <img src="assets/packages/DEFAULT/icons/icon.png" alt="Teak-icon">
+                    <div class="div-title-card">
+                        <h2 class="title-card">DEFAULT TWEAK</h2>
+                    </div>
+                    <div class="div-info-card">
+                        <h2 class="kind-card">Tweak</h2>
+                        <h2 class="user-card">Camorobot</h2>
+                    </div>
+                </div>
+            </div>
+        </sectopn>
+    </section>
+</body>
+</html>
